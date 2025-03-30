cb13ae406481add21f9a8770fb3458664e3d9162
Ethan Jansen <ethancjansen@gmail.com>
2025-03-30 15:59:45 -0700

    removed TODO comment

--- db.py
+++ db.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
     if not connector.testConnection():
         sys.exit(2)
 
-    # test data -- TODO: test failing operations
+    # test data
     testEventDT = datetime(2022, 4, 28, 14, 21)
     testInputEvent = {
         "subject": "Battery below 50%!",
